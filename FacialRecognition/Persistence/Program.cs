aa4ccebfd2ecf5ed78de33ac1e9961b39cffde97
Akme <you@example.com>
2021-12-05 21:49:44 -0400

    Cositas2

--- FacialRecognition/Persistence/Program.cs
+++ FacialRecognition/Persistence/Program.cs
@@ -17,6 +17,7 @@ namespace Persistence
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+
         }
     }
 }
