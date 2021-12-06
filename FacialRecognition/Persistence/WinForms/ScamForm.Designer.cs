bae5b8a3136cc5829ea1e02ae36260099f580219
Psycho Thinker <paraquezada@gmail.com>
2021-12-05 22:05:40 -0400

    La vida es triste

---
+++ FacialRecognition/Persistence/WinForms/ScamForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Persistence.WinForms
+{
+    partial class ScamForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(117, 164);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // ScamForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 516);
+            this.Controls.Add(this.panel1);
+            this.Name = "ScamForm";
+            this.Text = "ScamForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
