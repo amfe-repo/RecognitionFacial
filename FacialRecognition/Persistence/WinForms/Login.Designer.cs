2aff7660d8dffb5e27fa26eb13d081ff57565f2c
Akme <you@example.com>
2021-12-05 21:42:08 -0400

    Cositas

--- FacialRecognition/Persistence/WinForms/Login.Designer.cs
+++ FacialRecognition/Persistence/WinForms/Login.Designer.cs
@@ -52,6 +52,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(500, 39);
             this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // pictureBox1
             // 
--- FacialRecognition/Persistence/WinForms/Login.cs
+++ FacialRecognition/Persistence/WinForms/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Persistence
 {
@@ -75,12 +76,24 @@ namespace Persistence
             {
                 textBox2.Text = "Password";
                 textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+
             }
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
