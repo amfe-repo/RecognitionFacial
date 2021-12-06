3de2da4dfbf8f4b839e331a87aab25deaa604404
Akme <you@example.com>
2021-12-06 01:12:37 -0400

    ddd

--- FacialRecognition/Persistence/WinForms/ScamForm.Designer.cs
+++ FacialRecognition/Persistence/WinForms/ScamForm.Designer.cs
@@ -53,7 +53,7 @@
             this.panel1.Controls.Add(this.lblMinimize);
             this.panel1.Controls.Add(this.lblExit);
             this.panel1.Location = new System.Drawing.Point(-2, -2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(832, 38);
             this.panel1.TabIndex = 0;
@@ -65,7 +65,7 @@
             this.lblMinimize.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMinimize.Location = new System.Drawing.Point(1022, 1);
             this.lblMinimize.Name = "lblMinimize";
-            this.lblMinimize.Size = new System.Drawing.Size(33, 45);
+            this.lblMinimize.Size = new System.Drawing.Size(28, 37);
             this.lblMinimize.TabIndex = 1;
             this.lblMinimize.Text = "-";
             this.lblMinimize.Click += new System.EventHandler(this.lblMinimize_Click);
@@ -76,7 +76,7 @@
             this.lblExit.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblExit.Location = new System.Drawing.Point(1062, 1);
             this.lblExit.Name = "lblExit";
-            this.lblExit.Size = new System.Drawing.Size(39, 45);
+            this.lblExit.Size = new System.Drawing.Size(33, 37);
             this.lblExit.TabIndex = 0;
             this.lblExit.Text = "X";
             this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
@@ -102,7 +102,7 @@
             this.label1.Location = new System.Drawing.Point(410, 46);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 45);
+            this.label1.Size = new System.Drawing.Size(119, 32);
             this.label1.TabIndex = 2;
             this.label1.Text = "Full name";
             // 
@@ -110,7 +110,7 @@
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(51, 166);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(262, 232);
             this.pictureBox1.TabIndex = 3;
@@ -123,7 +123,7 @@
             this.label2.Location = new System.Drawing.Point(410, 137);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 45);
+            this.label2.Size = new System.Drawing.Size(56, 32);
             this.label2.TabIndex = 4;
             this.label2.Text = "Age";
             // 
@@ -134,7 +134,7 @@
             this.label3.Location = new System.Drawing.Point(410, 230);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(177, 45);
+            this.label3.Size = new System.Drawing.Size(130, 32);
             this.label3.TabIndex = 5;
             this.label3.Text = "Enrollment";
             // 
@@ -145,7 +145,7 @@
             this.label4.Location = new System.Drawing.Point(410, 322);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(158, 45);
+            this.label4.Size = new System.Drawing.Size(116, 32);
             this.label4.TabIndex = 6;
             this.label4.Text = "First dose";
             // 
@@ -156,7 +156,7 @@
             this.label5.Location = new System.Drawing.Point(410, 416);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(205, 45);
+            this.label5.Size = new System.Drawing.Size(151, 32);
             this.label5.TabIndex = 7;
             this.label5.Text = "Second dose";
             // 
@@ -166,7 +166,7 @@
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox1.Location = new System.Drawing.Point(416, 85);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(400, 36);
             this.textBox1.TabIndex = 8;
@@ -177,7 +177,7 @@
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox2.Location = new System.Drawing.Point(416, 177);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(400, 36);
             this.textBox2.TabIndex = 9;
@@ -188,7 +188,7 @@
             this.textBox3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox3.Location = new System.Drawing.Point(416, 270);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(400, 36);
             this.textBox3.TabIndex = 10;
@@ -199,7 +199,7 @@
             this.textBox4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox4.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox4.Location = new System.Drawing.Point(416, 362);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(400, 36);
             this.textBox4.TabIndex = 11;
@@ -210,7 +210,7 @@
             this.textBox5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox5.Location = new System.Drawing.Point(416, 456);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(400, 36);
             this.textBox5.TabIndex = 12;
@@ -234,7 +234,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ScamForm";
             this.Text = "ScamForm";
             this.panel1.ResumeLayout(false);
