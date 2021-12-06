25db94b8617ae4af85d429c66f4917e9d12de87d
Psycho Thinker <paraquezada@gmail.com>
2021-12-05 22:40:46 -0400

    Se creó el ScamForm

--- FacialRecognition/Persistence/Program.cs
+++ FacialRecognition/Persistence/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persistence.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,7 +18,6 @@ namespace Persistence
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
-
         }
     }
 }
